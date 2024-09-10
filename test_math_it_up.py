7638a91a3d216a8b248d3dcb9beba4b1695e7b50
sammy2706 <sibdah@icloud.com>
2024-09-10 13:25:11 -0600

    I completed the code.

--- test_math_it_up.py
+++ test_math_it_up.py
@@ -31,23 +31,35 @@ def test_is_even():
   """
   Tests for the `is_even` function
   """
+  assert math_it_up.is_even(2) == True
+  assert math_it_up.is_even(7) == False
+ 
 
 def test_is_odd():
   """
   Tests for the `is_odd` function
   """
+  assert math_it_up.is_odd(43) == True
+  assert math_it_up.is_odd(2) == False
+  
 
 def test_mean():
   """
   Tests for the `mean` function
   """
+  assert math_it_up.mean([5, 2, 4, 5]) == 4
+  assert math_it_up.mean([7, 9, 1, 4, 5]) == 5.2
 
 def test_median():
   """
   Tests for the `median` function
   """
+  assert math_it_up.median([1, 6, 8, 3, 9]) == 6
+  assert math_it_up.median([1, 4, 9, 5, 7, 6, 4]) == 5
 
 def test_mode():
   """
   Tests for the `mode` function
   """
+  assert math_it_up.mode([1, 7, 7 ,8, 9, 7, 7]) == [7]
+  assert math_it_up.mode([1, 2, 9, 4, 8, 73, 9, 9, 9, 9]) == [9]
